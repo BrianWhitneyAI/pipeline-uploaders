b7d0d6078a0ec9340a3f6fc6ab8457cb787a56be
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2022-02-25 11:28:01 -0800

    Changed format of EMT uploader

--- pipeline_uploaders/emt_uploader.py
+++ pipeline_uploaders/emt_uploader.py
@@ -1,7 +1,6 @@
 import os
-from aicsfiles import FileManagementSystem
+from numpy import NaN
 from fms_uploader import FMSUploader
-from pathlib import Path
 
 
 '''
@@ -16,106 +15,61 @@ Starting code base for EMT Uploader
 
         NOTE: Super class for uploaders 
         NOTE: look into helper constructor
-
-
-init: (self, dir_path, env = 'stg)
-    self.env = 'env'
-    define barcode 
-    files to upload (path)
-    list of dictionaries with metadata for each filepath in files to upload
-
-    for dirpath, _, filenames in os.walk(dir_path):
-        for filename in filenames:
-            if filename contains barcode
-                if .czi
-                    if 10x
-                        barcode
-                        well
-                        magnification
-                        labkey metadata
-                    elif blockduration 
-                        barcode
-                        well = MXS or labkey
-                        magnification = 63 
-                        timepoint
-                        labkey metadata
-                elif .czmbi
-                    Barcode 
-                    labkey metadata
-
-            elif .czexp
-                Barcode
-                labkey metadata
-
-
 '''
-class EMTUploader(FMSUploader):
-    BLOCK = "AcquisitionBlock"
+class EMTUploader():
 
-    def __init__(self, file_path: str, env = 'stg'):
-        self.barcode  = int(file_path.name[:9]) # this is oversimplified at moment 
-        upload_files = []
-        file_metadata  = [] 
+    def __init__(self, dir_path: str, env = 'stg'):
+
+        BLOCK = "AcquisitionBlock"
+        self.env = env
+        self.barcode  = int(dir_path.name[:9]) # this is oversimplified at moment 
+        self.files = []
+
         for dirpath, _, filenames in os.walk(dir_path):
             for filename in filenames:
+                file_path = f'{dirpath}/{filename}'
                 if str(self.barcode) in filename:
                     if '.czi' in filename:
                         if '10x' in filename:
-                            upload_file.append(f'{dirpath}/{filename}')
-                            well = filename.split("_")[-1]
-                            magnification = 10
-                            # labkey metadata
-                            metadata = {
-                                            "microsocpy": {
-                                                "plate_barcode": [barcode],  
-                                                "EMT": {
-                                                    "well": [well],
-                                                    "magnification": [magnification]
-                                                },
-                                            },
-                                        }
-                            file_metadata.append(metadata)
+                            # Labkey Metadata
+                            self.files.append(self.metadata_formatter(filename = file_path,
+                                                                 file_type = 'czi',
+                                                                 well = filename.split("_")[-1], 
+                                                                 magnification = 10))
                         elif BLOCK in filename:
-                            upload_file.append(f'{dirpath}/{filename}')
-                            # well = MXS or labkey
-                            magnification = 63 
                             timepoint = int([s for s in filename.split('_') if BLOCK in s][0][-1])
-                            # labkey metadata
-                            metadata = {
-                                            "microsocpy": {
-                                                "plate_barcode": [barcode],  
-                                                "EMT": {
-                                                    "well": [well],
-                                                    "magnification": [magnification],
-                                                    "timepoint": [timepoint]
-                                                },
-                                            },
-                                        }
-                            file_metadata.append(metadata)
+                            # Labkey Metadata
+                            self.files.append(self.metadata_formatter(filename = file_path,
+                                                                 file_type = 'czi',
+                                                                 magnification = 63,
+                                                                 timepoint = timepoint))
                     elif '.czmbi' in filename:
-                        upload_file.append(f'{dirpath}/{filename}')
-                        #labkey metadata
-                        metadata = {
-                                        "microsocpy": {
-                                            "plate_barcode": [barcode],  
-                                            "EMT": {
-                                            },
-                                        },
-                                    }
-                        file_metadata.append(metadata)
-            elif '.czexp' in filename:
-                upload_file.append(f'{dirpath}/{filename}')
-                #labkey metadata
-                metadata = {
-                                "microsocpy": {
-                                    "plate_barcode": [barcode],  
-                                    "EMT": {
-                                    },
-                                },
-                            }
-                file_metadata.append(metadata)
+                        self.files.append(self.metadata_formatter(filename = file_path, file_type = 'czmbi'))
 
-        for file_path, metadata in zip(upload_files,file_metadata):
-            file_with_metadata = super.__init__(self.file_path, self.file_type, self.metadata, self.env)
-            file_with_metadata.upload()
+                elif '.czexp' in filename:
+                    self.files.append(self.metadata_formatter(filename = file_path, file_type = 'czexp'))
 
+
+    def metadata_formatter(self, filename:str,file_type: str, well = NaN, magnification = NaN, timepoint = NaN):
+
+        # this can change for however metadata is wanted to be formatted 
+        metadata = {
+                    "microsocpy": {
+                        "plate_barcode": [self.barcode],  
+                        "EMT": {
+                            "well": [well],
+                            "magnification": [magnification],
+                            "timepoint": [timepoint]
+                        },
+                    },
+                }
+        
+        return FMSUploader( file_path = filename, 
+                            file_type  = file_type,
+                            metadata  = metadata , 
+                            env = self.env)
+
+
+    def upload(self):
+        for file in self.files:
+            file.upload()
