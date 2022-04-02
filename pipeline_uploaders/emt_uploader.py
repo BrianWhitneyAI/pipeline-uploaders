d6e88c10dc9430f81ea90704c148c9620bbe4756
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2022-04-01 17:07:59 -0700

    metadata formatting for EMT

--- pipeline_uploaders/emt_uploader.py
+++ pipeline_uploaders/emt_uploader.py
@@ -1,4 +1,5 @@
 import os
+from re import A
 import xml.etree.ElementTree as ET
 from pathlib import Path
 from xml.etree.ElementTree import tostring as xml_to_string
@@ -37,7 +38,7 @@ class EMTUploader:
 
         # Creates a List of all active wells and a Dictionary of Scene-Well relationship
         if not aqusition_block_1_paths:
-            self.wells, self.scene_dict = self.get_well_data(
+            self.wells, self.scene_dict, self.rows, self.cols = self.get_well_data(
                 str(aqusition_block_1_paths[0])
             )
         else:
@@ -47,23 +48,27 @@ class EMTUploader:
         This code block is a series if logic statements
         to pick filetype and assign necessary metadata
         """
+        self.well_ids = []
+        r = FMSUploader.get_labkey_metadata(self.barcode)
+        for row,col in zip(self.rows,self.cols):
+            self.well_ids.append(FMSUploader.get_well_id(r,row,col))
+
         for dirpath, _, filenames in os.walk(dir_path):
             for filename in filenames:
                 file_path = f"{dirpath}/{filename}"
                 if str(self.barcode) in filename:
                     if ".czi" in filename:
+                        imaging_date = get_imaging_date(file_path)
                         if "10x" in filename:
-                            # Labkey Metadata
-                            # pipeline 4 annotations
+
                             self.files.append(
                                 self.metadata_formatter(
                                     barcode=self.barcode,
                                     filename=file_path,
-                                    file_type="czi",
-                                    well=filename.split("_")[
-                                        -1
-                                    ],  # Need standard way to get Well ID
-                                    magnification=10,
+                                    file_type="czi", # needs to chage
+                                    imaging_date = self.imaging_date,
+                                    well_ids=self.well_ids,  # Need standard way to get Well ID
+                                    objective=10, # objectiive
                                     env=self.env,
                                 )
                             )
@@ -71,16 +76,14 @@ class EMTUploader:
                             timepoint = int(
                                 [s for s in filename.split("_") if BLOCK in s][0][-1]
                             )
-                            # Labkey Metadata
-                            # pipeline 4 annotations
-
                             self.files.append(
                                 self.metadata_formatter(
                                     barcode=self.barcode,
                                     filename=file_path,
                                     file_type="czi",
-                                    well=self.wells,
-                                    magnification=63,
+                                    imaging_date = self.imaging_date,
+                                    well_ids =self.well_ids,
+                                    objective=63,
                                     timepoint=timepoint,
                                     env=self.env,
                                 )
@@ -93,7 +96,8 @@ class EMTUploader:
                                 barcode=self.barcode,
                                 filename=file_path,
                                 file_type="czmbi",
-                                well=self.wells,
+                                imaging_date = self.imaging_date,
+                                well_ids =self.well_ids,
                                 env=self.env,
                             )
                         )
@@ -106,7 +110,8 @@ class EMTUploader:
                             barcode=self.barcode,
                             filename=file_path,
                             file_type="czexp",
-                            well=self.wells,
+                            imaging_date = self.imaging_date,
+                            well_ids=self.well_ids,
                             env=self.env,
                         )
                     )
@@ -116,19 +121,25 @@ class EMTUploader:
         barcode: str,
         filename: str,
         file_type: str,
-        well: str,
+        well_ids: str,
         env: str,
-        magnification=None,
+        imaging_date: str,
+        objective=None,
         timepoint=None,
     ):
 
         # this can change for however metadata is wanted to be formatted
+        # microscopy.wellid, micoroscoy.imaging_date, micorcospy.fov_id, micorsocpy.objective, microsocpy.plate_barcode
+        #
+        r = FMSUploader.get_labkey_metadata(barcode)
+
         metadata = {
-            "microsocpy": {
+            "microscopy": {
+                "well_ids" : [well_ids],
+                "imaging_date" : [imaging_date],
+                "objective": [objective],
                 "plate_barcode": [barcode],
                 "EMT": {
-                    "well": [well],
-                    "magnification": [magnification],
                     "timepoint": [timepoint],
                 },
             },
@@ -142,8 +153,21 @@ class EMTUploader:
     def get_well_data(block_path):
         block_img = AICSImage(block_path)
 
+        row_code = {
+                'A': 1,
+                'B': 2,
+                'C': 3,
+                'D': 4,
+                'E': 5,
+                'F': 6,
+                'G': 7,
+                'H': 8,
+        }
+
         scene_dict = {}
         wells = []
+        rows = []
+        cols =  []
 
         with open("metadata.czi.xml", "w") as f:
             f.write(xml_to_string(block_img.metadata, encoding="unicode"))
@@ -158,7 +182,28 @@ class EMTUploader:
             if (Scene.find("Shape").get("Name")) not in wells:
                 wells.append(Scene.find("Shape").get("Name"))
 
-        return wells, scene_dict
+        for well in wells:
+            rows.append(int(row_code[well[0]]))
+            cols.append(int(well[1:]))
+
+        return wells, scene_dict, rows, cols
+
+    def get_imaging_date(file_path):
+        # path = './ImageDocument/Metadata/Information/Image/AcquisitionDateAndTime'
+        file_img = AICSImage(file_path)
+
+        with open("metadata.czi.xml", "w") as f:
+            f.write(xml_to_string(file_img.metadata, encoding="unicode"))
+        tree = ET.parse("metadata.czi.xml")
+        root = tree.getroot()
+
+        return tree.findall(".//AcquisitionDateAndTime")[0].text 
+
+
+
+
+
+
 
     def upload(self):
         for file in self.files:
