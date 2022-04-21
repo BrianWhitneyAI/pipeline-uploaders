0b52c624c39219937deaca2cc4be22276b3d8d3c
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2022-04-21 11:21:35 -0700

    new versioning

--- aics_pipeline_uploaders/__init__.py
+++ aics_pipeline_uploaders/__init__.py
@@ -12,7 +12,7 @@ __author__ = "Brian Whitney"
 __email__ = "brian.whitney@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.0"
+__version__ = "1.0.1"
 
 
 def get_module_version():
