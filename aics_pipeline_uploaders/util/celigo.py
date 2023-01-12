9e35ac7afb93e61e674f7317323b0dbd9188a824
chughes <chrishu@alleninstitute.org>
2023-01-12 14:48:05 -0800

    Handling for 404, 500, invalid wellName

--- aics_pipeline_uploaders/util/celigo.py
+++ aics_pipeline_uploaders/util/celigo.py
@@ -17,16 +17,22 @@ class CeligoUtil:
         self.env = env
 
     def lookup_well_id(self, plate_barcode, well_name):
-        url = ENV_SERVICE_MAP[self.env][SERVICE_MMS][MMS_RESOURCE_PLATE_INFO] + str(
-            plate_barcode
-        )
-        well_info_response = requests.get(url)
-        well_info = well_info_response.json()
-        if len(well_info["data"]) > 1:
-            raise BarcodeException(f"Barcode {plate_barcode} is used by more than one plate.")
-        well_name_lookup = well_info["data"][0]["wellNameLookup"]
-
-        return well_name_lookup[well_name]["wellId"]
+        url = ENV_SERVICE_MAP[self.env][SERVICE_MMS][MMS_RESOURCE_PLATE_INFO] + str(plate_barcode)
+        response: requests.Response = requests.get(url)
+        if(response.status_code == 404):
+            return None
+        elif(response.status_code == 200):
+            barcode_info = response.json()
+            if len(barcode_info['data']) > 1:
+                raise BarcodeException(f"Barcode {plate_barcode} is used by more than one plate.")
+            well_name_lookup = barcode_info['data'][0]['wellNameLookup']
+            if(well_name in well_name_lookup):
+                well_id = well_name_lookup[well_name]['wellId']
+            else:
+                well_id = None
+            return well_id
+        else:
+            raise MMSException(f"Error requesting barcode info from MMS: {response.status_code}")
 
     def parse_filename(self, file_name):
         raw_metadata = file_name.split("_")
@@ -55,4 +61,7 @@ class CeligoUtil:
         return plate_barcode, well_name, scan_date, scan_time
 
 class BarcodeException(Exception):
+    pass
+
+class MMSException(Exception):
     pass
